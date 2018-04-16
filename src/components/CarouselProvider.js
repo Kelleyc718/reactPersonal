73e9000be747efa296698935283a29dae79d642c
Chris Kelley <kelleyc718@gmail.com>
2018-04-16 12:49:38 -0500

    fixed some site img links

--- src/components/CarouselProvider.js
+++ src/components/CarouselProvider.js
@@ -14,16 +14,17 @@ export default class extends React.Component {
         <Source />
         <Slider>
           <Slide index={0}>
-            <Image src="https://thumbs.gfycat.com/KeenElectricGelada-size_restricted.gif" />
+            <Image src="https://thumbs.gfycat.com/UnhappyGiganticHomalocephale-size_restricted.gif" />
           </Slide>
           <Slide index={1}>
-            <Image src="https://thumbs.gfycat.com/IncompatibleAbandonedGlassfrog-size_restricted.gif" />
+
           </Slide>
           <Slide index={2}>
-            <Image src="https://cdp.azureedge.net/products/USA/DU/2017/MC/SUPERSPORT/SUPERBIKE_959_PANIGALE/50/ARCTIC_WHITE_SILK/2000000003.jpg" />
+            <Image src="https://thumbs.gfycat.com/KeenElectricGelada-size_restricted.gif" />
+
           </Slide>
           <Slide index={3}>
-            <Image src="https://cdp.azureedge.net/products/USA/DU/2017/MC/SUPERSPORT/SUPERBIKE_959_PANIGALE/50/ARCTIC_WHITE_SILK/2000000003.jpg" />
+            <Image src="https://thumbs.gfycat.com/IncompatibleAbandonedGlassfrog-size_restricted.gif" />
           </Slide>
         </Slider>
         <ButtonBack onClick={this.handleChange}>Back</ButtonBack>
--- src/components/source.js
+++ src/components/source.js
@@ -5,52 +5,52 @@ import '../css/porfolio.css';
 
 const titles =
 [
-    {
-      title: "Contacts Manager",
-      lang: <i className="devicon-java-plain-wordmark"></i>,
-      sUrl:
-        <a href="https://github.com/emma-chris/contactsManager">
-          <i className="devicon-github-plain-wordmark"></i>
-        </a>,
-        desc: " Simple phone contact demo using Java 9."
-    },
-    {
-      title: "Github File List Utility",
-      lang:
-        <i className="devicon-javascript-plain"></i>,
-      sUrl:
-        <a href="https://github.com/Kelleyc718/GHFileLister">
-          <i className="devicon-github-plain-wordmark"></i>
-        </a>,
-        desc: " A utility made for fun. Uses Github's v3 REST API to access a users public repo and list the files. Recursively checks folder contents as well."
-    },
-    {
-      title: "Weather App",
-      lang: <span>
-        <i className="devicon-javascript-plain"></i>
-        <i className="devicon-webpack-plain colored"></i>
+  {
+    title: "Adlister (Craigslist Clone)",
+    lang:
+    <span>
+      <i className="devicon-amazonwebservices-original"></i>
+        <i class="devicon-java-plain"></i>
+        <i class="devicon-mysql-plain"></i>
+        <i class="devicon-bootstrap-plain"></i>
       </span>,
-      sUrl:
-        <a href="https://github.com/Kelleyc718/codeup-web-exercises/tree/master/deploy/weatherMap">
-          <i className="devicon-github-plain-wordmark"></i>
-        </a>,
-        desc: "Weather App utilizing Open Weather API, Google Maps API, IP API. Uses users originating IP address to populate initial weather. Also plays with Material IO's design tools."
-    },
-    {
-      title: "Adlister (Craigslist Clone)",
-      lang:
-      <span>
-        <i className="devicon-amazonwebservices-original"></i>
-          <i class="devicon-java-plain"></i>
-          <i class="devicon-mysql-plain"></i>
-          <i class="devicon-bootstrap-plain"></i>
-        </span>,
-      sUrl:
-        <a href="http://tbclister-env.smz2mnhxpg.us-east-1.elasticbeanstalk.com/ads">
-          <i className="devicon-devicon-plain"></i>
-        </a>,
-        desc: " Full stack MVC project providing persistance through a Relational dB. The site is hosted on AWS's ElasticBeanstalk and data is stored on AWS's RDS using MySQL 5.6. The server is being run using AWS's EC2 service running Tomcat on Linux. To add a layer of fun to the project, design was completed using only Bootstrap 4 classes."
-    }
+    sUrl:
+      <a href="http://tbclister-env.smz2mnhxpg.us-east-1.elasticbeanstalk.com/ads">
+        <i className="devicon-devicon-plain"></i>
+      </a>,
+      desc: " Full stack MVC project providing persistance through a Relational dB. The site is hosted on AWS's ElasticBeanstalk and data is stored on AWS's RDS using MySQL 5.6. The server is being run using AWS's EC2 service running Tomcat on Linux. To add a layer of fun to the project, design was completed using only Bootstrap 4 classes."
+  },
+  {
+    title: "Weather App",
+    lang: <span>
+      <i className="devicon-javascript-plain"></i>
+      <i className="devicon-webpack-plain colored"></i>
+    </span>,
+    sUrl:
+      <a href="https://github.com/Kelleyc718/codeup-web-exercises/tree/master/deploy/weatherMap">
+        <i className="devicon-github-plain-wordmark"></i>
+      </a>,
+      desc: "Weather App utilizing Open Weather API, Google Maps API, IP API. Uses users originating IP address to populate initial weather. Also plays with Material IO's design tools."
+  },
+  {
+    title: "Contacts Manager",
+    lang: <i className="devicon-java-plain-wordmark"></i>,
+    sUrl:
+      <a href="https://github.com/emma-chris/contactsManager">
+        <i className="devicon-github-plain-wordmark"></i>
+      </a>,
+    desc: " Simple phone contact demo using Java 9."
+  },
+  {
+    title: "Github File List Utility",
+    lang:
+      <i className="devicon-javascript-plain"></i>,
+    sUrl:
+      <a href="https://github.com/Kelleyc718/GHFileLister">
+        <i className="devicon-github-plain-wordmark"></i>
+      </a>,
+      desc: " A utility made for fun. Uses Github's v3 REST API to access a users public repo and list the files. Recursively checks folder contents as well."
+  }
 ]
 
 class Source extends React.Component {
